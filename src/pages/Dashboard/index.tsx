46f8c2278da76803e8fcab172e4af024c4ca7eb7
Jamessom Queiroz <jamessomqueiroz@gmail.com>
2020-08-15 02:26:52 -0300

    Styling dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,115 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi'
 
-import { Title } from './styles';
+import logo from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>
+  return (
+    <>
+      <img src={logo} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="/#">
+          <img
+            src="https://avatars1.githubusercontent.com/u/2079263?s=460&u=8b81f496c0242cd7e8de7a7a0847277449288d42&v=4"
+            alt="Jamessom Queiroz"
+          />
+          <div>
+            <strong>jamessom/rawrails</strong>
+            <p>Using docker and docker-compose to run rails app </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="/#">
+          <img
+            src="https://avatars1.githubusercontent.com/u/2079263?s=460&u=8b81f496c0242cd7e8de7a7a0847277449288d42&v=4"
+            alt="Jamessom Queiroz"
+          />
+          <div>
+            <strong>jamessom/rawrails</strong>
+            <p>Using docker and docker-compose to run rails app </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="/#">
+          <img
+            src="https://avatars1.githubusercontent.com/u/2079263?s=460&u=8b81f496c0242cd7e8de7a7a0847277449288d42&v=4"
+            alt="Jamessom Queiroz"
+          />
+          <div>
+            <strong>jamessom/rawrails</strong>
+            <p>Using docker and docker-compose to run rails app </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="/#">
+          <img
+            src="https://avatars1.githubusercontent.com/u/2079263?s=460&u=8b81f496c0242cd7e8de7a7a0847277449288d42&v=4"
+            alt="Jamessom Queiroz"
+          />
+          <div>
+            <strong>jamessom/rawrails</strong>
+            <p>Using docker and docker-compose to run rails app </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="/#">
+          <img
+            src="https://avatars1.githubusercontent.com/u/2079263?s=460&u=8b81f496c0242cd7e8de7a7a0847277449288d42&v=4"
+            alt="Jamessom Queiroz"
+          />
+          <div>
+            <strong>jamessom/rawrails</strong>
+            <p>Using docker and docker-compose to run rails app </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="/#">
+          <img
+            src="https://avatars1.githubusercontent.com/u/2079263?s=460&u=8b81f496c0242cd7e8de7a7a0847277449288d42&v=4"
+            alt="Jamessom Queiroz"
+          />
+          <div>
+            <strong>jamessom/rawrails</strong>
+            <p>Using docker and docker-compose to run rails app </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="/#">
+          <img
+            src="https://avatars1.githubusercontent.com/u/2079263?s=460&u=8b81f496c0242cd7e8de7a7a0847277449288d42&v=4"
+            alt="Jamessom Queiroz"
+          />
+          <div>
+            <strong>jamessom/rawrails</strong>
+            <p>Using docker and docker-compose to run rails app </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
